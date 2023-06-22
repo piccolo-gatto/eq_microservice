a9db89b4f7c919ddc0941621cc76cf15c6f19a6c
Ekaterina <108530800+piccolo-gatto@users.noreply.github.com>
2023-06-22 22:50:24 +0800

    Update comments

--- main.py
+++ main.py
@@ -4,20 +4,19 @@ import secrets
 from pydantic import EmailStr
 from fastapi import FastAPI, UploadFile
 from datetime import datetime
-from uuid import uuid4
 
 from app.upload_logic import FileStorage
 
 api = FastAPI()
 
 @api.post("/upload_file")
-def upload_file(file: UploadFile, user_mail: EmailStr, start_datetime: datetime, end_datetime: datetime): #необходимо реализовать получение токена
+def upload_file(file: UploadFile, user_mail: EmailStr, start_datetime: datetime, end_datetime: datetime):
 	user_id = 0
-	#тут будет поиск мейла в бд, если всё в порядке, получаем id пользователя, запоминаем в переменную user_id
+	#поиск почты в бд, если всё в порядке, получаем id пользователя, запоминаем в переменную user_id
 	storage = FileStorage()
 	token = secrets.token_hex(FileStorage.TOKEN_LENGTH)
 	path = storage.make_processing_dir(token)
-	#with open(path, "wb") as f:
-        #    	f.write(file.read())
+	with open(path, "wb") as f:
+            	f.write(file.read())
 	#запись в бд
 	return {'status': 'ok', 'user_id': user_id, 'path': path, 'start_datetime': start_datetime, 'end_datetime': end_datetime}
