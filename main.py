d5d2758a1662ba4bfc14df6235313884ccc34f62
Ekaterina <108530800+piccolo-gatto@users.noreply.github.com>
2023-06-22 22:47:32 +0800

    Update API for upload files

--- main.py
+++ main.py
@@ -1,36 +1,23 @@
+import os
+import secrets
+
 from pydantic import EmailStr
 from fastapi import FastAPI, UploadFile
+from datetime import datetime
 from uuid import uuid4
 
-#from logic import user_exists, authorisation
+from app.upload_logic import FileStorage
 
 api = FastAPI()
 
-
-@api.post("/register")
-async def register(user_name: str, user_mail: EmailStr, password:str):
-    token = uuid4()
-    #проверка существования в бд
-    if user_exists(user_name, user_mail):
-        return {"status": "failed", "message":"The user already exists"}
-    #добавление
-    #
-    return {"status":"ok", "user_name": user_name,
-            "user_mail": user_mail, "token":token}
-
-@api.post("/log_in")
-async def log_in(user_mail: EmailStr, password:str):
-    #проверка существования
-    #
-    #if !authorisation(user_mail,password):
-    #    return {"status": "failed", "message":"You are not registered"}
-    return {"status":"ok", "token":token}
-
 @api.post("/upload_file")
-def upload_file(file: UploadFile): #необходимо реализовать получение токена
+def upload_file(file: UploadFile, user_mail: EmailStr, start_datetime: datetime, end_datetime: datetime): #необходимо реализовать получение токена
 	user_id = 0
-	#тут будет проверка на корректность токена, если всё в порядке, получаем id пользователя, запоминаем в переменную user_id
-	filepath = 'data/' + str(user_id) + '/' + file.filename
-	with open(filepath, "wb") as f:
-            	f.write(file.read())
-	return {'status': 'ok', 'filepath': filepath}
+	#тут будет поиск мейла в бд, если всё в порядке, получаем id пользователя, запоминаем в переменную user_id
+	storage = FileStorage()
+	token = secrets.token_hex(FileStorage.TOKEN_LENGTH)
+	path = storage.make_processing_dir(token)
+	#with open(path, "wb") as f:
+        #    	f.write(file.read())
+	#запись в бд
+	return {'status': 'ok', 'user_id': user_id, 'path': path, 'start_datetime': start_datetime, 'end_datetime': end_datetime}
