c6e06e9054cd7debf83736a97c7be7d1ef8c9df3
злата <118795314+dzhoshua@users.noreply.github.com>
2023-06-25 21:25:55 +0800

    Add models.py with models User and Uploaded_file

---
+++ app/models.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String, DateTime, Text, ForeignKey
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    email = Column(String)
+
+    files = relationship("Uploaded_file", back_populates="user")
+
+
+class Uploaded_file(Base):
+    __tablename__ = "uploaded_files"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    path = Column(Text)
+    start_datetime = Column(DateTime)
+    end_datetime = Column(DateTime)
+
+    user = relationship("User", back_populates="files")
