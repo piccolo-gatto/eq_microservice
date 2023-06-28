dbb4b2118a86665ed225dcdae2af0ecfb69e4fa7
злата <118795314+dzhoshua@users.noreply.github.com>
2023-06-28 19:13:51 +0800

    Add tests for endpoint create_user

--- tests/test_create_user.py
+++ tests/test_create_user.py
@@ -1,10 +1,16 @@
-import app.logic as logic
+import app.crud as crud
 import app.models as models
 import app.schemas as schemas
 from app.database import SessionLocal
 import pytest
+from fastapi.testclient import TestClient
+
+from app.main import api
+
+client = TestClient(api)
 
 class TestUserCreation():
+    
     def test_get_user_by_email(self):
         email = "test@example.com"
         user = models.User(email=email)
@@ -13,22 +19,29 @@ class TestUserCreation():
         db.commit()
         db.refresh(user)
 
-        retrieved_user = logic.get_user_by_email(db, email=email)
+        retrieved_user = crud.get_user_by_email(db, email=email)
         assert retrieved_user.email == email
 
     def test_create_user(self):
-        email = "test@example.com"
+        email = "test2@example.com"
         user = schemas.UserCreate(email=email)
         db = SessionLocal()
 
-        created_user = logic.create_user(db, user=user)
+        created_user = crud.create_user(db, user=user)
         assert created_user.email == email
 
-        retrieved_user = logic.get_user_by_email(db, email=email)
+        retrieved_user = crud.get_user_by_email(db, email=email)
         assert retrieved_user is not None
         assert retrieved_user.email == email
 
-    
-    def test_create_user_fail(self):
-        email = "fail"
-        user = schemas.UserCreate(email=email)
+    def test_api_create_user(self):
+        user = {"email": "example1@example.com"}
+        response = client.post("/create_user", json=user)
+        assert response.status_code == 200
+        assert response.json() == {"email": "example1@example.com"}
+
+    def test_api_create_user_fail(self):
+        user = {"email": "example1@example.com"}
+        response = client.post("/create_user", json=user)
+        assert response.status_code == 400
+        assert response.json() == {"detail": "Email already registered"}
