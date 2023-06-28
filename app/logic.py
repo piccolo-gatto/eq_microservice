9b151ffd4fbbf6676eb41fb9ed267a7fec1c90e7
злата <118795314+dzhoshua@users.noreply.github.com>
2023-06-28 10:13:59 +0800

    Add funcs for token, update create_user

--- app/logic.py
+++ app/logic.py
@@ -1,13 +1,24 @@
 from sqlalchemy.orm import Session
 from pydantic import BaseModel, EmailStr
+import secrets
 
 from . import models, schemas
 
 def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
 
-def create_user(db: Session, user: schemas.UserCreate):
-    db_user = models.User(email=user.email)
+def get_id_by_token(db: Session, token: str):
+    user = db.query(models.User).filter(models.User.token == token).first()
+    return user.id
+
+
+def generate_token():
+    token = secrets.token_hex(16)
+    return token
+
+def create_user(db: Session, user: schemas.UserToke):
+    token = generate_token()
+    db_user = models.User(email=user.email, token=token)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
