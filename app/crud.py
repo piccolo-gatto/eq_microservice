b0976f7505131df9f84a9fc5079c34d3340551c7
злата <118795314+dzhoshua@users.noreply.github.com>
2023-06-28 23:59:45 +0800

    Add logging

--- app/crud.py
+++ app/crud.py
@@ -1,18 +1,29 @@
 from sqlalchemy.orm import Session
+from fastapi import HTTPException
 from pydantic import BaseModel, EmailStr
 import secrets
+from loguru import logger
 
 from . import models, schemas
 
 def get_user_by_email(db: Session, email: str):
-    return db.query(models.User).filter(models.User.email == email).first()
+    try:
+        db_user = db.query(models.User).filter(models.User.email == email).first()
+    except:
+        logger.error("Error receiving user data by email")
+        raise HTTPException(status_code=500, detail="Error receiving user data by email")
+    return db_user
 
 def get_id_by_token(db: Session, token: str):
-    user = db.query(models.User).filter(models.User.token == token).first()
+    try:
+        user = db.query(models.User).filter(models.User.token == token).first()
+    except:
+        logger.error("Error receiving user data by token")
+        raise HTTPException(status_code=500, detail="Error receiving user data by token")
     return user.id
 
-
 def generate_token():
+    logger.info("Generating a token")
     token = secrets.token_hex(16)
     return token
 
@@ -22,4 +33,5 @@ def create_user(db: Session, user: schemas.UserToke):
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
+    logger.info(f"New DB user {db_user.token} was successfully created")
     return db_user
