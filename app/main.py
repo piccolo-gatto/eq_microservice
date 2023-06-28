40c3769da176b840e0230112b8c71ffeaee5de17
злата <118795314+dzhoshua@users.noreply.github.com>
2023-06-28 17:29:37 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from pydantic import EmailStr
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 
-from . import models, schemas, logic
+from . import models, schemas, crud
 from .database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
@@ -20,8 +20,8 @@ def get_db():
 #Endpoints
 @api.post("/create_user", response_model=schemas.User)
 async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = logic.get_user_by_email(db, email=user.email)
+    db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     #добавление
-    return logic.create_user(db=db, user=user)
+    return crud.create_user(db=db, user=user)
