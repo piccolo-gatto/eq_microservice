f5a710198781e1dd0db19281101b57e45afb3fa0
Ekaterina <108530800+piccolo-gatto@users.noreply.github.com>
2023-06-30 13:14:49 +0800

    Update tests for upload files

--- tests/test_upload_files.py
+++ tests/test_upload_files.py
@@ -25,22 +25,24 @@ class TestUploadFiles():
         db.commit()
         assert retrieved_token == token
 
-	
-
     def test_get_ivent_by_token(self):
         datetime_start = '2023-11-12 04:11:47' 
         datetime_end = '2023-11-12 04:15:36'
         token = logic.generate_token()
+        ivent = models.Ivent(datetime_start=datetime_start, datetime_end=datetime_end, token=token)
         db = SessionLocal()
-        logic.create_ivent(db, datetime_start=datetime_start, datetime_end=datetime_end, token=token)
-	
+        db.add(ivent)
+        db.commit()
+        db.refresh(ivent)
         retrieved_ivent = logic.get_ivent_by_token(db, token=token)
+        db.delete(ivent)
+        db.commit()
         assert retrieved_ivent.datetime_start == datetime_start
         assert retrieved_ivent.datetime_end == datetime_end
 
     def test_api_upload_file(self):
-        URL = "/upload_file?email=ekaterina@gmail.com&type=roti&datetime_start=2023-11-12 04:11:47&datetime_end=2023-11-12 04:15:36"
-        file= {'file': ('test.txt', open('test_files/test.txt', 'rb'), "text/plain")}
-        response = client.post(URL, file=file)
+        URL = "/upload_file?email=test%40gmail.com&type=text&datetime_start=2023-01-01%2000%3A00%3A00&datetime_end=2023-01-01%2000%3A10%3A00"
+        file= {'file': ('test.txt', open('./test_files/test.txt', 'rb'), "text/plain")}
+        response = client.post(URL, files=file)
         assert response.status_code == 200
         
