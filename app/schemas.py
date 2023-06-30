7f3cbebfc000e4415c5e353051a7a3e88fd0ab95
Ekaterina <108530800+piccolo-gatto@users.noreply.github.com>
2023-06-30 13:59:29 +0800

    Refactor code

---
+++ app/schemas.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel,EmailStr
+from datetime import datetime
+import secrets
+
+
+class File(BaseModel):
+    id: int
+    user_id: int
+    ivent_id: int
+    type: str
+    path: str
+    datetime: str
+
+    class Config:
+        orm_mode = True
+
+class UserBase(BaseModel):
+    email: EmailStr
+
+
+class UserCreate(UserBase):
+    pass
+
+
+class User(UserBase):
+    id: int
+    files: list[File] = []
+
+    class Config:
+        orm_mode = True
+
+
